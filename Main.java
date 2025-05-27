00af9efc6201a81a777a23113319c0ecdd57519f
mcamock <mcamock@met.edu>
2025-05-27 10:46:33 +0530

    Initial commit with project files

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins! This is a test Java program.");
+        
+        // Call the test method
+        testMethod();
+    }
+    
+    public static void testMethod() {
+        // This is a simple test method
+        System.out.println("This is a test method!");
+    }
+}
